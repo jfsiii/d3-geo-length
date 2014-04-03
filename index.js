320cddd268a33acb8642e66f54f6b04d878f70de
John Schulz <jschulz@twitter.com>
2014-04-03 08:31:12 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,135 @@
+(function() {
+    !function() {
+        var d3 = {
+            version: "3.4.4"
+        };
+        function d3_noop() {}
+        var abs = Math.abs;
+        var π = Math.PI, τ = 2 * π, halfπ = π / 2, ε = 1e-6, ε2 = ε * ε, d3_radians = π / 180, d3_degrees = 180 / π;
+        function d3_sgn(x) {
+            return x > 0 ? 1 : x < 0 ? -1 : 0;
+        }
+        function d3_cross2d(a, b, c) {
+            return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0]);
+        }
+        function d3_acos(x) {
+            return x > 1 ? 0 : x < -1 ? π : Math.acos(x);
+        }
+        function d3_asin(x) {
+            return x > 1 ? halfπ : x < -1 ? -halfπ : Math.asin(x);
+        }
+        function d3_sinh(x) {
+            return ((x = Math.exp(x)) - 1 / x) / 2;
+        }
+        function d3_cosh(x) {
+            return ((x = Math.exp(x)) + 1 / x) / 2;
+        }
+        function d3_tanh(x) {
+            return ((x = Math.exp(2 * x)) - 1) / (x + 1);
+        }
+        function d3_haversin(x) {
+            return (x = Math.sin(x / 2)) * x;
+        }
+        d3.geo = {};
+        d3.geo.stream = function(object, listener) {
+            if (object && d3_geo_streamObjectType.hasOwnProperty(object.type)) {
+                d3_geo_streamObjectType[object.type](object, listener);
+            } else {
+                d3_geo_streamGeometry(object, listener);
+            }
+        };
+        function d3_geo_streamGeometry(geometry, listener) {
+            if (geometry && d3_geo_streamGeometryType.hasOwnProperty(geometry.type)) {
+                d3_geo_streamGeometryType[geometry.type](geometry, listener);
+            }
+        }
+        var d3_geo_streamObjectType = {
+            Feature: function(feature, listener) {
+                d3_geo_streamGeometry(feature.geometry, listener);
+            },
+            FeatureCollection: function(object, listener) {
+                var features = object.features, i = -1, n = features.length;
+                while (++i < n) d3_geo_streamGeometry(features[i].geometry, listener);
+            }
+        };
+        var d3_geo_streamGeometryType = {
+            Sphere: function(object, listener) {
+                listener.sphere();
+            },
+            Point: function(object, listener) {
+                object = object.coordinates;
+                listener.point(object[0], object[1], object[2]);
+            },
+            MultiPoint: function(object, listener) {
+                var coordinates = object.coordinates, i = -1, n = coordinates.length;
+                while (++i < n) object = coordinates[i], listener.point(object[0], object[1], object[2]);
+            },
+            LineString: function(object, listener) {
+                d3_geo_streamLine(object.coordinates, listener, 0);
+            },
+            MultiLineString: function(object, listener) {
+                var coordinates = object.coordinates, i = -1, n = coordinates.length;
+                while (++i < n) d3_geo_streamLine(coordinates[i], listener, 0);
+            },
+            Polygon: function(object, listener) {
+                d3_geo_streamPolygon(object.coordinates, listener);
+            },
+            MultiPolygon: function(object, listener) {
+                var coordinates = object.coordinates, i = -1, n = coordinates.length;
+                while (++i < n) d3_geo_streamPolygon(coordinates[i], listener);
+            },
+            GeometryCollection: function(object, listener) {
+                var geometries = object.geometries, i = -1, n = geometries.length;
+                while (++i < n) d3_geo_streamGeometry(geometries[i], listener);
+            }
+        };
+        function d3_geo_streamLine(coordinates, listener, closed) {
+            var i = -1, n = coordinates.length - closed, coordinate;
+            listener.lineStart();
+            while (++i < n) coordinate = coordinates[i], listener.point(coordinate[0], coordinate[1], coordinate[2]);
+            listener.lineEnd();
+        }
+        function d3_geo_streamPolygon(coordinates, listener) {
+            var i = -1, n = coordinates.length;
+            listener.polygonStart();
+            while (++i < n) d3_geo_streamLine(coordinates[i], listener, 1);
+            listener.polygonEnd();
+        }
+        d3.geo.length = function(object) {
+            d3_geo_lengthSum = 0;
+            d3.geo.stream(object, d3_geo_length);
+            return d3_geo_lengthSum;
+        };
+        var d3_geo_lengthSum;
+        var d3_geo_length = {
+            sphere: d3_noop,
+            point: d3_noop,
+            lineStart: d3_geo_lengthLineStart,
+            lineEnd: d3_noop,
+            polygonStart: d3_noop,
+            polygonEnd: d3_noop
+        };
+        function d3_geo_lengthLineStart() {
+            var λ0, sinφ0, cosφ0;
+            d3_geo_length.point = function(λ, φ) {
+                λ0 = λ * d3_radians, sinφ0 = Math.sin(φ *= d3_radians), cosφ0 = Math.cos(φ);
+                d3_geo_length.point = nextPoint;
+            };
+            d3_geo_length.lineEnd = function() {
+                d3_geo_length.point = d3_geo_length.lineEnd = d3_noop;
+            };
+            function nextPoint(λ, φ) {
+                var sinφ = Math.sin(φ *= d3_radians), cosφ = Math.cos(φ), t = abs((λ *= d3_radians) - λ0), cosΔλ = Math.cos(t);
+                d3_geo_lengthSum += Math.atan2(Math.sqrt((t = cosφ * Math.sin(t)) * t + (t = cosφ0 * sinφ - sinφ0 * cosφ * cosΔλ) * t), sinφ0 * sinφ + cosφ0 * cosφ * cosΔλ);
+                λ0 = λ, sinφ0 = sinφ, cosφ0 = cosφ;
+            }
+        }
+        if (typeof define === "function" && define.amd) {
+            define(d3);
+        } else if (typeof module === "object" && module.exports) {
+            module.exports = d3;
+        } else {
+            this.d3 = d3;
+        }
+    }();
+})();
